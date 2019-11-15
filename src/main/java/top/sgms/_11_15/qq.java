e49b39386c3487b4f8c88ce8deab50ede2cb4f77
smallbo <smallbo0913@163.com>
2019-11-15 15:36:42 +0800

    你好

---
+++ src/main/java/top/sgms/_11_15/qq.java
@@ -0,0 +1,14 @@
+package top.sgms._11_15;
+
+/**
+ * ClassName: qq
+ * Description:
+ * Date:      2019/11/15 15:36
+ * author     xiaobo
+ * version    V1.0
+ */
+public class qq {
+    public static void main(String[] args) {
+        System.out.println("as gks");
+    }
+}
