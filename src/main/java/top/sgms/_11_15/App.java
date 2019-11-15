5da2492585598c7885383471e821305d6eb99f95
shenguimosheng12345 <2468892573@qq.com>
2019-11-15 15:29:44 +0800

    Initial commit

---
+++ src/main/java/top/sgms/_11_15/App.java
@@ -0,0 +1,13 @@
+package top.sgms._11_15;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
